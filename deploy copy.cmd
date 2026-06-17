25ea8546d89611ecd0d878dd49028a47a61e6b01
okcanvas <93498208+okcanvas@users.noreply.github.com>
2026-06-17 09:03:24 +0900

    Update portfolio

---
+++ deploy copy.cmd
@@ -0,0 +1,21 @@
+cd D:\okcanvas.github.io
+
+
+git fetch origin
+
+git worktree add --detach ..\okcanvas-gh-pages
+cd ..\okcanvas-gh-pages
+
+git checkout --orphan gh-pages
+git rm -rf . >nul 2>nul
+
+xcopy "D:\okcanvas.github.io\docs\.vitepress\dist\*" "." /E /I /Y
+
+type nul > .nojekyll
+
+git add .
+git commit -m "Deploy portfolio"
+git push -u origin gh-pages --force
+
+cd D:\okcanvas.github.io
+git worktree remove ..\okcanvas-gh-pages --force
